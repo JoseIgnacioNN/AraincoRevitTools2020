a35a6f34d8ff2d3ec0a44dcd8de50144bd938fea
José Ignacio Núñez Navarrete <61503887+JoseIgnacioNN@users.noreply.github.com>
2023-09-14 00:43:16 -0300

    Update script.py

--- Arainco Tools R20.tab/Experimental.panel/Experimental1.stack/Emplantillado.pushbutton/script.py
+++ Arainco Tools R20.tab/Experimental.panel/Experimental1.stack/Emplantillado.pushbutton/script.py
@@ -54,20 +54,24 @@ class CategorySelectionFilter(ISelectionFilter):
 # Define un filtro para seleccionar elementos de la categoría "Structural Foundations"
 filter = CategorySelectionFilter()
 
-# Utiliza el método PickObjects para permitir al usuario seleccionar elementos.
-picked_elements = uidoc.Selection.PickObjects(ObjectType.Element, filter, "Selecciona elementos de la categoría 'Structural Foundations'")
+try:
+    # Utiliza el método PickObjects para permitir al usuario seleccionar elementos.
+    picked_elements = uidoc.Selection.PickObjects(ObjectType.Element, filter, "Selecciona elementos de la categoría 'Structural Foundations'")
+    
+    if picked_elements:
+        # Recopila los ElementId de los elementos seleccionados
+        selected_element_ids = [reference.ElementId for reference in picked_elements]
 
-if picked_elements:
-    # Recopila los ElementId de los elementos seleccionados
-    selected_element_ids = [reference.ElementId for reference in picked_elements]
+        # Convierte la lista de ElementId en una colección List[ElementId]
+        selected_element_ids_collection = List[ElementId](selected_element_ids)
 
-    # Convierte la lista de ElementId en una colección List[ElementId]
-    selected_element_ids_collection = List[ElementId](selected_element_ids)
+        # Establece los elementos seleccionados en el documento actual
+        uidoc.Selection.SetElementIds(selected_element_ids_collection)
 
-    # Establece los elementos seleccionados en el documento actual
-    uidoc.Selection.SetElementIds(selected_element_ids_collection)
-
-    # Ahora los elementos permanecerán seleccionados y puedes manipularlos posteriormente
-else:
-    # El usuario canceló la selección o no se seleccionaron elementos.
-    TaskDialog.Show("Sin selección", "No se seleccionaron elementos de la categoría 'Structural Foundations'.")
+        # Ahora los elementos permanecerán seleccionados y puedes manipularlos posteriormente
+    else:
+        # No se seleccionaron elementos.
+        TaskDialog.Show("Sin selección", "No se seleccionaron elementos de la categoría 'Structural Foundations'.")
+except OperationCanceledException:
+    # El usuario presionó el botón "Escape" para cancelar la selección.
+    TaskDialog.Show("Selección cancelada", "El usuario canceló la selección de elementos.")
